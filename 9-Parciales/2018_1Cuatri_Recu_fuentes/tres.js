527cc7f06ebc4022fdbd32035361d94a8d9603fa
WildeGaf <40776504+WildeGaf@users.noreply.github.com>
2018-07-05 21:58:18 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,17 @@
 function mostrar()
 {
-
+var precio;
+var descuento;
+var cuenta;
+var	iva;
+var total;
+var resta;
+precio = prompt ("Ingrese el precio");
+descuento = prompt ("Ingrese el % de descuento");
+iva = (precio * 21 / 100);
+cuenta = (precio * descuento / 100);
+resta = (precio - cuenta);
+total = (precio - cuenta - iva);
+alert("el descuento es de "+cuenta+" $ el precio con el descuento es de "+resta+" $ El iva es de"+iva);
+document.getElementById('elPrecioFinal').value = total;
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
-
+var base;
+var alt;
+var lado;
+var suma;
+base = prompt("Ingrese la base");
+alt = prompt("Ingrese la altura");
+base = parseint(base);
+alt = parseint(alt)
+lado = (alt * 2);
+suma = (base + lado);
+sup = (base * alt / 2);
+alert ("El perimetro es de "+suma"y la superficie es de "+sup);
 }
